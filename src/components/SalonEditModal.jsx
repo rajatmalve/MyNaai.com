28197a0bdbc54801ca61642b69d8fed5aa054a88
Rajat Malve <rajatmalve9@gmail.com>
2025-12-26 12:48:53 +0530

    add day

--- src/components/SalonEditModal.jsx
+++ src/components/SalonEditModal.jsx
@@ -479,12 +479,12 @@ const SalonEditModal = ({ showModal, setShowModal, modalMode, editingSalonId, on
                     disabled={isSubmitting}
                   >
                     <option value="">Select Day</option>
-                    {/* <option value="monday">Monday</option>
+                    <option value="monday">Monday</option>
                     <option value="tuesday">Tuesday</option>
                     <option value="wednesday">Wednesday</option>
                     <option value="thursday">Thursday</option>
                     <option value="friday">Friday</option>
-                    <option value="saturday">Saturday</option> */}
+                    <option value="saturday">Saturday</option>
                     <option value="sunday">Sunday</option>
                   </select>
                 </div>
